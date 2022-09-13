8fbfc1c0f7ad823114fc9459c03b8061655b45e1
LeonLow97 <102566827+LeonLow97@users.noreply.github.com>
2022-09-13 13:33:14 +0800

    first commit

---
+++ be/src/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello Team 6!")
+	jieWeiSaysHi()
+	amosSaysHi()
+}
+
+func jieWeiSaysHi() {
+	fmt.Println("Jie Wei was here!")
+}
+
+func amosSaysHi() {
+	fmt.Println("I am adlv boy")
+}
