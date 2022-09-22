3df6c6d3a6255f301f109615863b86f36da68934
Leon Low <leonlow@email.com>
2022-09-22 12:25:00 +0800

    completed admin update user frontend (svelte)

--- be/src/main.go
+++ be/src/main.go
@@ -22,13 +22,12 @@ func main() {
 	router.Use(cors.Default())
 
 	router.POST("/login", route.Login)
-
 	// router.POST("/admin-update-user", middleware.CheckCookie, route.AdminUpdateUser)
 	router.POST("/admin-update-user", route.AdminUpdateUser)
-	router.POST("/get-selected-users", route.GetSelectedUser)
-
+	// router.POST("/get-selected-users", route.GetSelectedUser)
 	router.POST("/admin-create-user", route.AdminCreateUser)
 	router.GET("/get-users", route.GetUsers)
+	router.GET("/get-user-groups", middleware.GetUserGroup)
 
 	port := middleware.LoadENV("SERVER_PORT")
 	server := fmt.Sprintf(":%v", port)
---
+++ fe/FE_src/src/AdminUpdateUserForm.svelte
@@ -0,0 +1,95 @@
+<script>
+	import axios from "axios"
+	import { toast } from '@zerodevx/svelte-toast'
+	import { SvelteToast } from '@zerodevx/svelte-toast'
+	import { onMount } from "svelte";
+	import MultiSelect from "svelte-multiselect"
+	import { Button, Form, FormGroup, FormText, Input, Label, ModalFooter } from 'sveltestrap';
+
+	let message = ""
+	let code = ""
+	export let username;
+	let password;
+	export let email;
+	export let user_group = [];
+	export let status
+
+	let groupsArray = [];
+	let selected = []
+	selected.push(...user_group)
+
+	async function handleSubmit() {
+		let user_group = selected.join(",")
+		const json = {username, password, email, user_group, status}
+
+		try {
+			const response = await axios.post("http://localhost:4000/admin-update-user", json)
+			if (response) {
+				console.log("POST BACK")
+				message = response.data.message
+				code = response.data.code
+				toast.push(message)
+				password = ""
+			}
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+    async function GetUserGroups() {
+      try {
+        const response = await axios.get(
+          "http://localhost:4000/get-user-groups"
+        );
+
+        if (response.data.error) {
+          console.log(response.data.error);
+        } else if (!response.data.error) {
+          const usergroups = response.data;
+
+          usergroups.forEach((group) => {
+            groupsArray.push(group);
+          });
+
+		  groupsArray = groupsArray
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    }
+    $: GetUserGroups();
+</script>
+
+<main>
+	<SvelteToast />
+</main>
+
+<style>
+</style>
+
+<form on:submit|preventDefault={handleSubmit}>
+<FormGroup>
+	<Label for="username">Username:</Label>
+	<Input type="text" bind:value={username} disabled />
+</FormGroup>
+<FormGroup>
+	<Label for="password">Password:</Label>
+	<Input type="password" bind:value={password} placeholder="Password" />
+</FormGroup>
+<FormGroup>
+	<Label for="email">Email:</Label>
+	<Input type="email" bind:value={email} placeholder="Email" />
+</FormGroup>
+<FormGroup>
+	<Label for="usergroup">User Group(s):</Label>
+	<MultiSelect bind:selected options={groupsArray} />
+</FormGroup>
+<FormGroup>
+    <Label for="status">Status</Label>
+    <Input type="select" bind:value={status} placeholder="Status">
+      <option>Inactive</option>
+      <option>Active</option>
+    </Input>
+</FormGroup>
+<Button color="primary" >Update User</Button>
+</form>
