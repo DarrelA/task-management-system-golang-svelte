2a5c6f2b90bffc7287d3090584b8da0eb8565f77
huien92 <huien92@yahoo.com.sg>
2022-10-03 10:50:41 +0800

    Updated get-plan.go

--- be/src/api/route/get-plan.go
+++ be/src/api/route/get-plan.go
@@ -1,4 +1,53 @@
 package route
 
+import (
+	"backend/api/middleware"
+	"backend/api/models"
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
 // route: /get-all-plans
-// remove this comment once added
+func GetAllPlans(c *gin.Context) {
+	// var plan models.Plan
+	// if err := c.BindJSON(&plan); err != nil {
+	//   return
+	// }
+	
+	var pData []models.Plan
+	var planName, planColor, planStartDate, planEndDate sql.NullString
+	// AppAcronym URL params will be passed in here
+	//var appAcro map[string][]string = c.Request.URL.Query()
+	planAcronym := "test"
+	rows, err := middleware.SelectAllPlans(planAcronym)
+	
+	if err != nil {
+		log.Fatalln("select query error", err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		err = rows.Scan(&planName, &planColor, &planStartDate, &planEndDate)
+
+		if err != nil {
+			log.Fatalln("scan error", err)
+		}
+
+		resArray := models.Plan {
+			PlanName: planName.String,
+			PlanAcronym: planAcronym,
+			PlanColor: planColor.String,
+			StartDate: planStartDate.String,
+			EndDate: planEndDate.String,
+		}
+
+		pData = append(pData, resArray)
+	}
+
+	c.JSON(200, pData)
+	fmt.Println("get plans!")
+}
