a942dc95377330c273f8c10870bed3e7c06505ac
Amos <amoschoo185@gmail.com>
2022-10-05 15:47:30 +0800

    updated

--- fe/FE_src/src/components/Kanban/Pages/MgtTask.svelte
+++ fe/FE_src/src/components/Kanban/Pages/MgtTask.svelte
@@ -1,10 +1,7 @@
 <script>
   import axios from "axios";
   import { errorToast, successToast } from "../../toast";
-<<<<<<< Updated upstream
   import {  Button, Card, CardBody, CardFooter, CardHeader, CardSubtitle, CardText, CardTitle, Row, Col } from "sveltestrap";
-=======
-  import { Table, Row, Col, Button, Modal, ModalBody, ModalHeader, ModalFooter } from "sveltestrap";
   import CreateTask from "../Form/CreateTask.svelte";
 
   let addTaskButton;
@@ -25,13 +22,11 @@
         task_notes = ""
         task_plan = ""
     }
->>>>>>> Stashed changes
 </script>   
 
 <style>
 </style>
 
-<<<<<<< Updated upstream
 
 <div class="text-center">
   <Row>
@@ -116,27 +111,4 @@
       </Card>
     </Col>
   </Row>
-=======
-<!-- This is dashboard where task(s) will be displayed after clicking into an application -->
-<!-- 1. Open -->
-
-<Modal isOpen={open} {toggleAddTask} {size}>
-  <ModalHeader {toggleAddTask}>Create Task</ModalHeader>
-  <ModalBody>
-      <CreateTask bind:this={addTaskButton} {task_name} {task_description} {task_notes} {task_plan} />
-  </ModalBody>
-  <ModalFooter>
-      <Button style="color: #fffbf0;" color="warning" on:click={(e) => addTaskButton.handleSubmit(e)}>Create Task</Button>
-      <Button class="back-button" color="danger" on:click={toggleAddTask}>Back</Button>
-  </ModalFooter>
-</Modal>
-
-<!-- 2. To Do -->
-<!-- 3. Doing -->
-<!-- 4. Done -->
-<!-- 5. Closed -->
-<div class="container-fluid">
-  <br/>
-  <Button style="float:right; font-weight: bold; color: black;" color="warning" on:click={toggleAddTask}>Create Task</Button>
->>>>>>> Stashed changes
 </div>
