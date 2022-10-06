0f46ec761ddfd472fd5cce6c2c58a89a9aa3a9b9
musk100 <amoschoo185@gmail.com>
2022-10-06 16:09:56 +0800

    merged latest

--- fe/FE_src/src/components/Kanban/Pages/Dashboard.svelte
+++ fe/FE_src/src/components/Kanban/Pages/Dashboard.svelte
@@ -6,21 +6,17 @@
     import AdminNavbar from "../../Admin/NavBar/IsLoggedInAdmin.svelte";
     import UserNavbar from "../../User/NavBar/IsLoggedInUser.svelte";
     import CreateTask from "../Form/CreateTask.svelte";
-    import MgtTask from "./MgtTask.svelte"
-    import MgtPlan from "./MgtPlan.svelte"
-    import Icon from "@iconify/svelte"
+    import MgtPlan from "../Pages/MgtPlan.svelte";
+    import MgtTask from "../Pages/MgtTask.svelte";
+    import Icon from '@iconify/svelte';
 
     const isAdmin = localStorage.getItem("isAdmin");
-    let addTaskButton;
 
     export let task_name = ""
     export let task_description = ""
     export let task_notes = ""
     export let task_plan = ""
     export let appacronym;
-
-    let size = "xl";
-    let open = false;
 
     function handleBack() {
         navigate("/home")
@@ -118,35 +114,9 @@
         <Col xs = "2">
             <MgtPlan appacronym={appacronym} />
         </Col>
-<<<<<<< HEAD
         <Col xs = "10">
-<<<<<<< HEAD
-            <MgtTask />
-=======
-        <Col>
-            <Button style="float:right; font-weight: bold; color: black;  margin-left: 10px;" color="warning" on:click={handleBack}>Back</Button> 
-            <!-- <Button style="float:right; font-weight: bold; color: black;" color="warning" on:click={toggleAddTask}>Add Task</Button> -->
-            <!-- <Button style="float:right; font-weight: bold; color: black;" color="warning" on:click={toggleUpdateTask}>Update Task</Button> -->
->>>>>>> 1c2ec3ec2d766e7af79147c1ce9bc26807a73b82
-=======
             <MgtTask appacronym={appacronym} />
->>>>>>> main
         </Col>
     </Row>
 </div>
 
-<<<<<<< HEAD
-<Modal isOpen={open} {toggleAddTask} {size}>
-    <ModalHeader {toggleAddTask}>Create Task</ModalHeader>
-    <ModalBody>
-        <CreateTask bind:this={addTaskButton} {task_name} {task_description} {task_notes} {task_plan} />
-    </ModalBody>
-    <ModalFooter>
-        <Button style="color: #fffbf0;" color="warning" on:click={(e) => addTaskButton.handleSubmit(e)}>Create Task</Button>
-        <Button class="back-button" color="danger" on:click={toggleAddTask}>Back</Button>
-    </ModalFooter>
-</Modal>
-=======
-<MgtTask />
-
->>>>>>> 1c2ec3ec2d766e7af79147c1ce9bc26807a73b82
