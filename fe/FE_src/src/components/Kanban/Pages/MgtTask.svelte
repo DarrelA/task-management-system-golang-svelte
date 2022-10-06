ed847f800035e54f6206599bc3c8d84d8bd30f76
Leon Low <lowjiewei@gmail.com>
2022-10-06 17:04:11 +0800

    resolved merge conflicts

--- fe/FE_src/src/components/Kanban/Pages/MgtTask.svelte
+++ fe/FE_src/src/components/Kanban/Pages/MgtTask.svelte
@@ -1,6 +1,6 @@
 <script>
-  import axios from 'axios';
-  import { errorToast, successToast } from '../../toast';
+  import axios from "axios";
+  import { errorToast, successToast } from "../../toast";
   import {
     Table,
     Row,
@@ -11,36 +11,19 @@
     ModalHeader,
     ModalFooter,
     Container,
-  } from 'sveltestrap';
-  import CreateTask from '../Form/CreateTask.svelte';
-  import Card from './Card.svelte';
-  import UpdateTask from '../Form/UpdateTask.svelte';
+  } from "sveltestrap";
+  import CreateTask from "../Form/CreateTask.svelte";
+  import Card from "./Card.svelte";
+  import UpdateTask from "../Form/UpdateTask.svelte";
+  
+  let appacronym;
+  let tasksData = "";
 
-  let addTaskButton;
+  // let userAppPermits = JSON.parse(localStorage.getItem('userAppPermits'));
+  // let { IsPermitCreate, IsPermitOpen, IsPermitToDo, IsPermitDoing, IsPermitDone } =
+  //   userAppPermits;
 
-  export let task_name = '';
-  export let task_description = '';
-  export let task_notes = '';
-  export let task_plan = '';
-  export let appacronym;
-  let tasksData = '';
-
-  let userAppPermits = JSON.parse(localStorage.getItem('userAppPermits'));
-  let { IsPermitCreate, IsPermitOpen, IsPermitToDo, IsPermitDoing, IsPermitDone } =
-    userAppPermits;
-
-  let size = 'lg';
-  let open = false;
-
-  function toggleAddTask(e) {
-    e.preventDefault();
-    open = !open;
-    task_name = '';
-    task_description = '';
-    task_notes = '';
-    task_plan = '';
-    GetAllTasks();
-  }
+  $: console.log(appacronym);
 
   async function GetAllTasks() {
     try {
@@ -52,105 +35,108 @@
       if (response.data.error) {
         console.log(response.data.error);
       } else if (!response.data.error) {
-        // console.log(response.data);
+        console.log(response.data);
         tasksData = response.data;
       }
     } catch (error) {
-      console.log(error);
+      console.log("error");
     }
   }
 
-  const GetUserAppPermits = async () => {
-    try {
-      const response = await axios.get(
-        `http://localhost:4000/get-user-app-permits?appacronym=${appacronym}`,
-        {
-          withCredentials: true,
-        }
-      );
-      if (response) {
-        localStorage.setItem('userAppPermits', JSON.stringify(response.data));
-      }
-    } catch (e) {
-      e.response && e.response.data.message
-        ? errorToast(e.response.data.message)
-        : errorToast(e.message);
-    }
-  };
+  // const GetUserAppPermits = async () => {
+  //   try {
+  //     const response = await axios.get(
+  //       `http://localhost:4000/get-user-app-permits?appacronym=${appacronym}`,
+  //       {
+  //         withCredentials: true,
+  //       }
+  //     );
+  //     if (response) {
+  //       localStorage.setItem('userAppPermits', JSON.stringify(response.data));
+  //     }
+  //   } catch (e) {
+  //     e.response && e.response.data.message
+  //       ? errorToast(e.response.data.message)
+  //       : errorToast(e.message);
+  //   }
+  // };
 
-  const demoteTask = async (task_name, task_state) => {
-    const json = { task_app_acronym: appacronym, task_name, task_state };
+  // const demoteTask = async (task_name, task_state) => {
+  //   const json = { task_app_acronym: appacronym, task_name, task_state };
 
-    try {
-      const response = await axios.put(
-        'http://localhost:4000/task-state-transition',
-        json,
-        {
-          withCredentials: true,
-        }
-      );
-      if (response) {
-        GetAllTasks();
-      }
-    } catch (e) {
-      e.response && e.response.data.message
-        ? errorToast(e.response.data.message)
-        : errorToast(e.message);
-    }
-  };
+  //   try {
+  //     const response = await axios.put(
+  //       'http://localhost:4000/task-state-transition',
+  //       json,
+  //       {
+  //         withCredentials: true,
+  //       }
+  //     );
+  //     if (response) {
+  //       GetAllTasks();
+  //     }
+  //   } catch (e) {
+  //     e.response && e.response.data.message
+  //       ? errorToast(e.response.data.message)
+  //       : errorToast(e.message);
+  //   }
+  // };
 
-  const promoteTask = async (task_name, task_state) => {
-    const json = { task_app_acronym: appacronym, task_name, task_state };
+  // const promoteTask = async (task_name, task_state) => {
+  //   const json = { task_app_acronym: appacronym, task_name, task_state };
 
-    try {
-      const response = await axios.put(
-        'http://localhost:4000/task-state-transition',
-        json,
-        {
-          withCredentials: true,
-        }
-      );
-      if (response) {
-        GetAllTasks();
-      }
-    } catch (e) {
-      e.response && e.response.data.message
-        ? errorToast(e.response.data.message)
-        : errorToast(e.message);
-    }
-  };
+  //   try {
+  //     const response = await axios.put(
+  //       'http://localhost:4000/task-state-transition',
+  //       json,
+  //       {
+  //         withCredentials: true,
+  //       }
+  //     );
+  //     if (response) {
+  //       GetAllTasks();
+  //     }
+  //   } catch (e) {
+  //     e.response && e.response.data.message
+  //       ? errorToast(e.response.data.message)
+  //       : errorToast(e.message);
+  //   }
+  // };
 
   $: GetAllTasks();
-  $: GetUserAppPermits();
+  // $: GetUserAppPermits();
 </script>
 
-{#if IsPermitCreate}
+<!-- {#if IsPermitCreate}
   <Button
     style="float:click={promoteTask}; font-weight: bold; color: black;"
     color="warning"
     on:click={toggleAddTask}>Create Task</Button
   >
-{/if}
+{/if} -->
+
+<CreateTask {appacronym} />
 
 <Row>
   <Col>
     Open
     {#each tasksData as task}
-      {#if task.task_state === 'Open'}
+      {#if task.task_state === "Open"}
         <Card>
           <span slot="task-name">{task.task_name}</span>
           <span slot="task-owner">{task.task_owner}</span>
           <span slot="task-description">{task.task_description}</span>
           <Row slot="task-actions">
-            {#if IsPermitOpen}
-              <Col><Button on:update-task><UpdateTask /></Button></Col>
-              <Col
-                ><Button
-                  color="primary"
-                  on:click={() => promoteTask(task.task_name, 'ToDo')}>&#8594;</Button
-                ></Col
-              >
-            {/if}
+            <!-- {#if IsPermitOpen} -->
+            <Col><Button on:update-task><UpdateTask /></Button></Col>
+            <Col
+              ><Button
+                color="primary"
+                on:click={() => promoteTask(task.task_name, "ToDo")}
+                >&#8594;</Button
+              ></Col
+            >
+            <!-- {/if} -->
           </Row>
         </Card>
       {/if}
@@ -160,21 +146,22 @@
   <Col>
     To Do
     {#each tasksData as task}
-      {#if task.task_state === 'ToDo'}
+      {#if task.task_state === "ToDo"}
         <Card>
           <span slot="task-name">{task.task_name}</span>
           <span slot="task-owner">{task.task_owner}</span>
           <span slot="task-description">{task.task_description}</span>
           <Row slot="task-actions">
-            {#if IsPermitToDo}
-              <Col><Button on:update-task><UpdateTask /></Button></Col>
-              <Col
-                ><Button
-                  color="primary"
-                  on:click={() => promoteTask(task.task_name, 'Doing')}>&#8594;</Button
-                ></Col
-              >
-            {/if}
+            <!-- {#if IsPermitToDo} -->
+            <Col><Button on:update-task><UpdateTask /></Button></Col>
+            <Col
+              ><Button
+                color="primary"
+                on:click={() => promoteTask(task.task_name, "Doing")}
+                >&#8594;</Button
+              ></Col
+            >
+            <!-- {/if} -->
           </Row>
         </Card>
       {/if}
@@ -185,27 +172,29 @@
   <Col>
     Doing
     {#each tasksData as task}
-      {#if task.task_state === 'Doing'}
+      {#if task.task_state === "Doing"}
         <Card>
           <span slot="task-name">{task.task_name}</span>
           <span slot="task-owner">{task.task_owner}</span>
           <span slot="task-description">{task.task_description}</span>
           <Row slot="task-actions">
-            {#if IsPermitDoing}
-              <Col
-                ><Button
-                  color="primary"
-                  on:click={() => demoteTask(task.task_name, 'ToDo')}>&#8592;</Button
-                ></Col
-              >
-              <Col><Button on:update-task><UpdateTask /></Button></Col>
-              <Col
-                ><Button
-                  color="primary"
-                  on:click={() => promoteTask(task.task_name, 'Done')}>&#8594;</Button
-                ></Col
-              >
-            {/if}
+            <!-- {#if IsPermitDoing} -->
+            <Col
+              ><Button
+                color="primary"
+                on:click={() => demoteTask(task.task_name, "ToDo")}
+                >&#8592;</Button
+              ></Col
+            >
+            <Col><Button on:update-task><UpdateTask /></Button></Col>
+            <Col
+              ><Button
+                color="primary"
+                on:click={() => promoteTask(task.task_name, "Done")}
+                >&#8594;</Button
+              ></Col
+            >
+            <!-- {/if} -->
           </Row>
         </Card>
       {/if}
@@ -215,27 +204,29 @@
   <Col>
     Done
     {#each tasksData as task}
-      {#if task.task_state === 'Done'}
+      {#if task.task_state === "Done"}
         <Card>
           <span slot="task-name">{task.task_name}</span>
           <span slot="task-owner">{task.task_owner}</span>
           <span slot="task-description">{task.task_description}</span>
           <Row slot="task-actions">
-            {#if IsPermitDone}
-              <Col
-                ><Button
-                  color="primary"
-                  on:click={() => demoteTask(task.task_name, 'Doing')}>&#8592;</Button
-                ></Col
-              >
-              <Col><Button on:update-task><UpdateTask /></Button></Col>
-              <Col
-                ><Button
-                  color="primary"
-                  on:click={() => promoteTask(task.task_name, 'Closed')}>&#8594;</Button
-                ></Col
-              >
-            {/if}
+            <!-- {#if IsPermitDone} -->
+            <Col
+              ><Button
+                color="primary"
+                on:click={() => demoteTask(task.task_name, "Doing")}
+                >&#8592;</Button
+              ></Col
+            >
+            <Col><Button on:update-task><UpdateTask /></Button></Col>
+            <Col
+              ><Button
+                color="primary"
+                on:click={() => promoteTask(task.task_name, "Closed")}
+                >&#8594;</Button
+              ></Col
+            >
+            <!-- {/if} -->
           </Row>
         </Card>
       {/if}
@@ -245,7 +236,7 @@
   <Col>
     Close
     {#each tasksData as task}
-      {#if task.task_state === 'Closed'}
+      {#if task.task_state === "Closed"}
         <Card>
           <span slot="task-name">{task.task_name}</span>
           <span slot="task-owner">{task.task_owner}</span>
@@ -255,28 +246,6 @@
     {/each}
   </Col>
 </Row>
-
-<Modal isOpen={open} {toggleAddTask} {size}>
-  <ModalHeader {toggleAddTask}>Create Task</ModalHeader>
-  <ModalBody>
-    <CreateTask
-      bind:this={addTaskButton}
-      {task_name}
-      {task_description}
-      {task_notes}
-      {task_plan}
-      {appacronym}
-    />
-  </ModalBody>
-  <ModalFooter>
-    <Button
-      style="color: #fffbf0;"
-      color="warning"
-      on:click={(e) => addTaskButton.handleSubmit(e)}>Create Task</Button
-    >
-    <Button class="back-button" color="danger" on:click={toggleAddTask}>Back</Button>
-  </ModalFooter>
-</Modal>
 
 <style>
 </style>
