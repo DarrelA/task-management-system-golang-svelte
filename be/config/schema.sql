a312cf5f239f62d0bab44f5192477316eb68021e
copipastaguy <narutoalf@yahoo.com>
2022-10-07 15:28:30 +0800

    updated sql schema, env copy to use

--- be/config/schema.sql
+++ be/config/schema.sql
@@ -1,5 +1,5 @@
 -- CREATE DATABASE AND TABLES
-
+DROP DATABASE c3_database;
 CREATE DATABASE IF NOT EXISTS c3_database;
 USE c3_database;
 
@@ -89,8 +89,29 @@ CREATE TABLE IF NOT EXISTS task_notes (
   CONSTRAINT task_name FOREIGN KEY (task_name) REFERENCES task (task_name)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO application (app_acronym, app_description, app_Rnum, app_startDate, app_endDate, app_permitCreate, app_permitOpen, app_permitToDo, app_permitDoing, app_permitDone, app_createdDate)
-VALUES ("apple","","2", NOW(), NOW(), null, null, null, null, null, NOW());
+
+-- DUMMY DATA FOR SQL
+INSERT INTO groupnames (user_group)
+VALUES ("Admin"), ("Project Lead"), ("Project Manager"), ("Team Member");
+
+INSERT INTO accounts (username, password, email, admin_privilege, user_group, status, timestamp)
+VALUES ("admin", "$2a$10$rYB8OEksGgnQ1twxAIrXnO.VF1VnbKfa1Pniw8l5kq4EdiZBKRZZK", "admin@tms.com", 1, "Admin", "Active", NOW()),
+("projectlead", "$2a$10$rYB8OEksGgnQ1twxAIrXnO.VF1VnbKfa1Pniw8l5kq4EdiZBKRZZK", "projectlead@tms.com", 1, "Project Lead", "Active", NOW()),
+("projectmanager", "$2a$10$rYB8OEksGgnQ1twxAIrXnO.VF1VnbKfa1Pniw8l5kq4EdiZBKRZZK", "projectmanager@tms.com", 1, "Project Manager", "Active", NOW()),
+("teammember", "$2a$10$rYB8OEksGgnQ1twxAIrXnO.VF1VnbKfa1Pniw8l5kq4EdiZBKRZZK", "teammember@tms.com", 1, "Team Member", "Active", NOW());
+
+INSERT INTO usergroup (username, user_group)
+VALUES ("admin", "Admin"),
+("projectlead", "Project Lead"),
+("projectmanager", "Project Manager"),
+("teammember", "Team Member");
 
 INSERT INTO application (app_acronym, app_description, app_Rnum, app_startDate, app_endDate, app_permitCreate, app_permitOpen, app_permitToDo, app_permitDoing, app_permitDone, app_createdDate)
-VALUES ("pear","","2", NOW(), NOW(), "Project Lead", "Project Manager", "Team Member", "Team Member", "Project Lead", NOW());
+VALUES ("apple","","2", NOW(), NOW(), NULL, NULL, NULL, NULL, NULL, NOW()),
+("pear","","2", NOW(), NOW(), "Project Lead", "Project Manager", "Team Member", "Team Member", "Project Lead", NOW());
+
+INSERT INTO plan (plan_mvp_name, plan_app_acronym, plan_color, plan_startDate, plan_endDate)
+VALUES ("sprint 1", "apple", "#dda15e", "2022-10-07", "2022-10-08");
+
+INSERT INTO task (task_app_acronym, task_id, task_name, task_description, task_notes, task_plan, task_color, task_state, task_creator, task_owner, task_createDate)
+VALUES ("apple", "apple_2", "microservice 1", "new task", "", "sprint 1", "#dda15e", "Open", "projectlead", "projectlead", NOW());
