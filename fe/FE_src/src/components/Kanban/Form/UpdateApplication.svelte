0c508725dee7ce21535298d67e67ce5fb8d110d8
musk100 <amoschoo185@gmail.com>
2022-10-07 09:42:40 +0800

    uodated 6/10/2022 not finished yet only outline

--- fe/FE_src/src/components/Kanban/Form/UpdateApplication.svelte
+++ fe/FE_src/src/components/Kanban/Form/UpdateApplication.svelte
@@ -1,8 +1,10 @@
 <script>
   import axios from "axios";
   import { onMount } from "svelte";
-  import { Form, FormGroup, Input, Label, Col, Row, Styles, Icon, Accordion, AccordionItem, Button, Modal, ModalHeader, ModalBody } from "sveltestrap";
+  import { Form, FormGroup, Input, Label, Col, Row, Styles, Icon, Accordion, AccordionItem, Button, Modal, ModalHeader, ModalBody, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from "sveltestrap";
+  import AddGroup from "../../Admin/Form/AddGroup.svelte";
   import { errorToast, successToast } from "../../toast";
+  import AddApplication from "./AddApplication.svelte";
 
   let app_startDate = ""
   let app_endDate = ""
@@ -12,6 +14,12 @@
   let app_permitDoing = ""
   let app_permitDone = ""
   let app_acronym = ""
+  let permitCreate = ""
+  let permitOpen = ""
+  let permitToDo = ""
+  let permitDoing = ""
+  let permitDone = ""
+  export let appacronym;
 
   let appData = "";
 
@@ -20,7 +28,7 @@
 
   export async function handleSubmit(event) {
     event.preventDefault()
-    const json = {app_acronym, app_startDate, app_endDate, app_permitCreate, app_permitOpen, app_permitToDo, app_permitDoing, app_permitDone}
+    const json = {app_acronym, app_startDate, app_endDate, permitCreate, permitOpen, permitToDo, permitDoing, permitDone}
     try {
       const response = await axios.post("http://localhost:4000/update-application", json, {withCredentials: true});
       setTimeout(() => {
@@ -28,11 +36,11 @@
          successToast(response.data.message);
          app_startDate = ""
          app_endDate = ""
-         app_permitCreate = ""
-         app_permitOpen = ""
-         app_permitToDo = ""
-         app_permitDoing = "" 
-         app_permitDone = ""
+         permitCreate = ""
+         permitOpen = ""
+         permitToDo = ""
+         permitDoing = ""
+         permitDone = ""
         }
       }, 500) 
     } catch(error) {
@@ -42,7 +50,7 @@
 
   async function GetApplicationData() {
     try {
-      const response = await axios.get("http://localhost:4000/get-application", { withCredentials: true });
+      const response = await axios.get(`http://localhost:4000/get-application?AppAcronym=${appacronym}`, { withCredentials: true });
       if (response.data.error) {
         console.log(response.data.error);
       } else if (!response.data.error) {
@@ -54,44 +62,65 @@
   }
   
   $: GetApplicationData()
-
-  function handleClick() {
-    open = !open
-  }
-
-  function toggle(e) {
-    e.preventDefault()
-    open = !open;
-  }
 </script>
-
+ 
 <style>
 </style>
 
-<Button style="font-weight: bold; color: black"; color="warning" on:click={handleClick} >Update Application</Button>
-
-<Modal isOpen={open} {toggle} {size}>
-  <ModalHeader {toggle}>Update Application</ModalHeader>
-  <ModalBody>
-    <Form>
-      <Row>
-        <Col>
+<Form>
+  <Row>
+    <Col>
+      <FormGroup>
+        <Label>App Acronym:</Label>
+        <Input type="text" value={appData.app_acronym} readonly/>
+      </FormGroup>
+    </Col>
+    <Col>
+      <FormGroup>
+        <Label>App Running Number:</Label>
+        <Input type="text" value={appData.app_Rnum} readonly/>
+      </FormGroup>
+    </Col>
+    <Row>
+      <FormGroup>
+        <Label>App Description:</Label>
+        <Input type="textarea" placeholder="App Description" rows={2} value={appData.app_description} readonly/>
+      </FormGroup>
+    </Row>
+    <Row>
+      <Col>
+        <FormGroup>
+          <Label>App Start Date:</Label>
+          <Input type="date" bind:value={app_startDate} />
+        </FormGroup>
+      </Col>
+      <Col>
+        <FormGroup>
+          <Label>App End Date:</Label>
+          <Input type="date" bind:value={app_endDate} />
+        </FormGroup>
+      </Col>
+    </Row>
+    <Row>
+      <Col>
+        <FormGroup>
+          <Dropdown>
+            <DropdownToggle style="width:100%" caret>App Permit Create</DropdownToggle>
+            <DropdownMenu>
+              {#each appData as app}
+                <DropdownItem on:click={() => (permitCreate = app.app_permitCreate)} placeholder={app}>
+                  {app.app_permitCreate} {app.app}
+                </DropdownItem>
+              {/each}
+            </DropdownMenu>
+          </Dropdown>
           <FormGroup>
-            <Label>Application Name</Label>
-            <Input type="text" bind:value={app_acronym} placeholder="Application Name" />
+            <Input value={permitCreate} type="text" readonly />
           </FormGroup>
-        </Col>
-        <Col>
-          <FormGroup>
-            <Label>Application Description</Label>
-            {appData.app_description}
-          </FormGroup>
-        </Col>
-      </Row>
-    </Form>
-  </ModalBody>
-</Modal>
-
-
+        </FormGroup>
+      </Col>
+    </Row>
+  </Row>
+</Form>
 
 
--- fe/FE_src/src/components/Kanban/Pages/Dashboard.svelte
+++ fe/FE_src/src/components/Kanban/Pages/Dashboard.svelte
@@ -1,6 +1,4 @@
 <script>
-    import axios from "axios";
-    import { errorToast, successToast } from "../../toast";
     import { Table, Row, Col, Button, Modal, ModalBody, ModalHeader, ModalFooter, Card, CardBody, CardSubtitle, CardText } from "sveltestrap";
     import { navigate } from "svelte-routing";
     import AdminNavbar from "../../Admin/NavBar/IsLoggedInAdmin.svelte";
@@ -9,30 +7,12 @@
     import MgtPlan from "../Pages/MgtPlan.svelte";
     import MgtTask from "../Pages/MgtTask.svelte";
     import Icon from '@iconify/svelte';
+     import MgtApp from "./MgtApp.svelte";
 
     const isAdmin = localStorage.getItem("isAdmin");
-
-    export let task_name = ""
-    export let task_description = ""
-    export let task_notes = ""
-    export let task_plan = ""
     export let appacronym;
 
-    function handleBack() {
-        navigate("/home")
-    }
 
-    function toggleAddTask(e) {
-        e.preventDefault()
-        open = !open;
-        task_name = ""
-        task_description = ""
-        task_notes = ""
-        task_plan = ""
-    }
-
-    let isOpen = false;
-    const toggle = () => (isOpen = !isOpen);
 </script>   
   
 <style>
@@ -44,68 +24,9 @@
     <UserNavbar />
 {/if}
 
-<div class="container-fluid">
-    <br/>
-
-    <Card>
-        <CardBody style="text-align: center;">
-            <Row>
-                <Col>
-                    <CardSubtitle>Application</CardSubtitle>
-                    <CardText>
-                        Application Name
-                    </CardText>
-                </Col>
-                <Col>
-                    <CardSubtitle>Permit Create</CardSubtitle>
-                    <CardText>
-                        Permit Create Username
-                    </CardText>
-                </Col>
-                <Col>
-                    <CardSubtitle>Permit Open</CardSubtitle>
-                    <CardText>
-                        Permit Open Username
-                    </CardText>
-                </Col>
-                <Col>
-                    <CardSubtitle>Permit To Do</CardSubtitle>
-                    <CardText>
-                        Permit To Do Username
-                    </CardText>
-                </Col>
-                <Col>
-                    <CardSubtitle>Permit Doing</CardSubtitle>
-                    <CardText>
-                        Permit Doing Username
-                    </CardText>
-                </Col>
-                <Col>
-                    <CardSubtitle>Permit Done</CardSubtitle>
-                    <CardText>
-                        Permit Done Username
-                    </CardText>
-                </Col>
-                <Col>
-                    <CardText>
-                        <Row>
-                            <Col>
-                                <Button style="font-weight: bold; color: black;" color="warning">
-                                    <Icon icon="bi:pencil-square" width="25" height="25" />
-                                </Button>
-                            </Col>
-                            <Col>
-                                <Button style="font-weight: bold; color: black;" color="warning" on:click={handleBack}>
-                                    <Icon icon="bi:arrow-left-square" width="25" height="25" />
-                                </Button>
-                            </Col>
-                        </Row>
-                    </CardText>
-                </Col>
-            </Row>
-        </CardBody>
-    </Card>
-</div>
+<Col xs = "15">
+    <MgtApp appacronym={appacronym} />
+</Col>
 
 <br/>
 
