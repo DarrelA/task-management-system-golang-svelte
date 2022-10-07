d1a2094e9863947bdb49040b82b70a0756ccdc34
huien92 <huien92@yahoo.com.sg>
2022-10-07 17:36:36 +0800

    Updated with create plan modal

--- fe/FE_src/src/components/Kanban/Pages/MgtPlan.svelte
+++ fe/FE_src/src/components/Kanban/Pages/MgtPlan.svelte
@@ -1,12 +1,25 @@
 <script>
   import axios from "axios";
   import { errorToast, successToast } from "../../toast";
-  import {  Button, Card, CardBody, CardHeader, CardSubtitle, CardText, CardTitle, Row, Col } from "sveltestrap";
+  import { Button, Card, CardBody, CardHeader, CardSubtitle, CardText, CardTitle, Row, Col } from "sveltestrap";
+  import { Modal, ModalHeader, ModalBody, ModalFooter } from "sveltestrap";
   import Icon from '@iconify/svelte';
   import Plan from "../Card/Plan.svelte";
+  import AddPlan from "../Form/AddPlan.svelte";
 
   export let appacronym = null;
   let plansData = [];
+
+  let openPlanModal = false;
+  let createPlanButton;
+  let buttonVisible = true;
+  let size = "xl";
+
+  let plan_name = "";
+  let plan_acronym = appacronym;
+  let plan_color = "";
+  let plan_start = "";
+  let plan_end = "";
 
   async function GetAllPlans() {
     try {
@@ -16,11 +29,22 @@
       );
 
       if (response.data) {
+        console.log(response.data)
         plansData = response.data;
       }
     } catch (error) {
       console.log("error");
     }
+  }
+
+  function toggleCreatePlan(e) {
+    e.preventDefault();
+    openPlanModal = !openPlanModal;
+    // plan_name = "";
+    // plan_color = "";
+    // plan_start = "";
+    // plan_end = "";
+    GetAllPlans();
   }
 
   $: GetAllPlans();
@@ -32,6 +56,8 @@
       <CardTitle>Plan</CardTitle>
     </CardHeader>
     <CardBody>
+      <!-- create plan test button -->
+      <Button size=small on:click={toggleCreatePlan}>Create plan</Button><br/><br/>
       <CardSubtitle>
       </CardSubtitle>
       <CardText>
@@ -47,6 +73,27 @@
     </CardBody>
   </Card>
 </div>
+
+<!-- Modal for Create Plan -->
+<Modal isOpen={openPlanModal} {toggleCreatePlan} {size}>
+  <ModalHeader {toggleCreatePlan}>Create Plan</ModalHeader>
+  <ModalBody>
+    <AddPlan bind:this=
+    {createPlanButton}
+    {appacronym}/>
+  </ModalBody>
+  <ModalFooter>
+    <Button
+      style="color: #fffbf0;"
+      color="warning"
+      on:click={(e) => createPlanButton.handleSubmit(e)}>
+      Create Plan
+    </Button>
+    <Button class="back-button" color="danger" on:click={toggleCreatePlan}>
+      Back
+    </Button>
+  </ModalFooter>
+</Modal>
 
 <style>
 </style>
